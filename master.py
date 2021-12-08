16669f5685553859eb2cdcc8f3647e2a97baab1e
KrishnadevL <krishnadevlaks@gmail.com>
2021-12-08 11:53:42 -0800

    returned to pre-directory due to issues of time, will be adding directory later

--- master.py
+++ master.py
@@ -1,21 +1,41 @@
 # import "packages" from flask
 from flask import render_template
 from __init__ import app
-
+from silmarillionmemes import getSilmMeme, noldolante
 #Pulls data for blueprints
 
 #About Pages
-from abt_pages.sophie.sophie import app_sophie
-from abt_pages.krish.krish import app_krish
 
 # connects default URL to render index.html
 @app.route('/')
 def index():
     return render_template("index.html")
 
+@app.route('/abt_pages/sophie')
+def sophie():
+    return render_template("abt_pages/sophie.html", SilmData=getSilmMeme(), noldolante=noldolante())
 
-app.register_blueprint(app_sophie)
-app.register_blueprint(app_krish)
+
+
+
+@app.route('/abt_pages/sophiealt')
+def sophiealt():
+    return render_template("abt_pages/sophiealt.html", noldolante=noldolante())
+
+
+@app.route('/abt_pages/krish/')
+def krish_abt():
+    return render_template("abt_pages/krish_abt.html")
+
+@app.route('/abt_pages/michael/')
+def michaelAbout():
+    return render_template("michaelAbout.html")
+
+@app.route('/abt_pages/valerie/')
+def valerie():
+    return render_template("val_about_me.html")
+
+
 
 # runs the application on the development server
 if __name__ == "__master__":
