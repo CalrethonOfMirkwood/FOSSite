a256fdc3d6c8ab10b94b19cd665eca206210ee26
KrishnadevL <krishnadevlaks@gmail.com>
2021-12-09 21:22:50 -0800

    Added navbar, website wide color scheme, personal api and journal link.

--- master.py
+++ master.py
@@ -1,7 +1,8 @@
 # import "packages" from flask
 from flask import Flask, render_template
+import requests
 from silmarillionmemes import getSilmMeme, noldolante
-
+import random
 # create a Flask instance
 app = Flask(__name__)
 
@@ -20,7 +21,29 @@ def sophiealt():
 
 @app.route('/abt_pages/krish/')
 def krish_abt():
-    return render_template("abt_pages/krish_abt.html")
+    # Link to public API, gets movie data by genre
+    url = ("https://data-imdb1.p.rapidapi.com/movie/byGen/Action/")
+
+    querystring = {"page_size":"50"}
+
+    headers = {
+        'x-rapidapi-host': "data-imdb1.p.rapidapi.com",
+        'x-rapidapi-key': "b399060d5cmshdde5a342e03dbfap1a4d84jsn99009368a5f2"
+    }
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+
+    print(response.text)
+    # Pulls results form data to then be sorted
+    movie_list = []
+    results = response.json().get('results')
+    # determines if there is a value within the list
+    for movie in results:
+        test = movie["title"]
+        movie_list.append(test)
+
+    return render_template("abt_pages/krish_abt.html", movie_choice=movie_list)
+
 
 @app.route('/abt_pages/michael/')
 def michaelAbout():
