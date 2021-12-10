7ada140cb40df8bef3f080c867ac17d3527c96c5
KrishnadevL <krishnadevlaks@gmail.com>
2021-12-09 19:16:08 -0800

    fixed errors and added updated layout page

--- master.py
+++ master.py
@@ -1,27 +1,22 @@
 # import "packages" from flask
-from flask import render_template
-from __init__ import app
+from flask import Flask, render_template
 from silmarillionmemes import getSilmMeme, noldolante
-#Pulls data for blueprints
 
-#About Pages
+# create a Flask instance
+app = Flask(__name__)
 
 # connects default URL to render index.html
 @app.route('/')
 def index():
     return render_template("index.html")
 
-@app.route('/abt_pages/sophie')
+@app.route('/abt_pages/sophie/')
 def sophie():
     return render_template("abt_pages/sophie.html", SilmData=getSilmMeme(), noldolante=noldolante())
 
-
-
-
-@app.route('/abt_pages/sophiealt')
+@app.route('/abt_pages/sophiealt/')
 def sophiealt():
     return render_template("abt_pages/sophiealt.html", noldolante=noldolante())
-
 
 @app.route('/abt_pages/krish/')
 def krish_abt():
@@ -29,23 +24,19 @@ def krish_abt():
 
 @app.route('/abt_pages/michael/')
 def michaelAbout():
-    return render_template("michaelAbout.html")
+    return render_template("abt_pages/michaelAbout.html")
 
 @app.route('/abt_pages/valerie/')
 def valerie():
-    return render_template("val_about_me.html")
-
-
+    return render_template("abt_pages/val_about_me.html")
 
 # runs the application on the development server
 if __name__ == "__master__":
     app.run()
 
-
 @app.errorhandler(500)
 def server_error(e):
     return render_template('500.html'), 500
-
 
 @app.errorhandler(404)
 def server_error(e):
