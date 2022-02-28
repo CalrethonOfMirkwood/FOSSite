4987149cc837ba6588c707e7ba25e832d1899a94
CalrethilOfMirkwood <82109882+CalrethilOfMirkwood@users.noreply.github.com>
2022-02-27 23:55:06 -0800

    not so quick fix

--- database/app.py
+++ database/app.py
@@ -1,13 +1,19 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
+file_path = os.path.abspath(os.getcwd())+'/database/myDB.db'
+
 """This needs to be isolated to support blueprints and models"""
 app = Flask(__name__)
-dbURI = 'sqlite:///database/myDB.db'
+dbURI = 'sqlite:///'+file_path
+
 # Setup properties for the database
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = dbURI
 app.config['SECRET_KEY'] = 'SECRET_KEY'
 db = SQLAlchemy(app)
 Migrate(app, db)
+print("!")
+print(dbURI)
